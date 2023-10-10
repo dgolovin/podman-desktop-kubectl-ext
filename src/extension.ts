35f4be241a54f9e8bd70e800e68d090a00a0f0a2
Denis Golovin <dgolovin@redhat.com>
2023-10-10 11:18:11 -0700

    fix: update CI build names and add Containerfile
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,5 @@
 import {ExtensionContext, cli} from '@podman-desktop/api';
 
 export function activate(context: ExtensionContext) {
-  cli.createCliTool({
-    id: "kubectl",
-    description: "A command line tool used to run commands against Kubernetes clusters. It does this by authenticating with the Master Node of your cluster and making API calls to do a variety of management actions. If you\'re just getting started with Kubernetes, prepare to be spending a lot of time with kubectl",
-    name: "kubectl",
-    displayName: "kubectl",
-    images: {
-      icon: 'icon.png'
-    }
-  })
+  // declared in package.json
 }
