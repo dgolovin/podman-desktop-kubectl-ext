9ad3f5c01e1f412a0aa256ed0a1aaca7ca017d04
Denis Golovin <dgolovin@redhat.com>
2023-10-11 16:37:13 -0700

    fix: use cli API to register CLI Tool
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,13 @@
 import {ExtensionContext, cli} from '@podman-desktop/api';
 
 export function activate(context: ExtensionContext) {
-  // declared in package.json
+  cli.createCliTool({
+    id: "kubectl",
+    markdownDescription: "A command line tool used to run commands against Kubernetes clusters. It does this by authenticating with the Master Node of your cluster and making API calls to do a variety of management actions. If you\'re just getting started with Kubernetes, prepare to be spending a lot of time with kubectl",
+    name: "kubectl",
+    displayName: "kubectl",
+    images: {
+      icon: 'icon.png'
+    }
+  })
 }
